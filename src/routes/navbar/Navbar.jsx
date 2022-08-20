31a8cde2f25d639187ec6b29c65bb0196305ce31
Nithin Gopal <nithingopal2015@gmail.com>
2022-08-20 10:23:55 +0530

    Fix Banner
    - added locations
    - added another phone

--- src/routes/navbar/Navbar.jsx
+++ src/routes/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
-import { MdPhoneInTalk } from 'react-icons/md'
+import { MdPhoneInTalk, MdLocationOn } from 'react-icons/md'
 import NavMenu from '../../containers/navMenu/NavMenu'
 import Footer from '../../containers/footer/Footer'
 
@@ -13,7 +13,18 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='ori__banner'><MdPhoneInTalk size={24} /><span><a href="tel:+">+91-9100094789</a></span></div>
+      <div className='ori__banner'>
+        <div className='ori__banner-location'>
+          <MdLocationOn size={20} />
+          <p>NELLORE / VIJAYAWADA</p>
+        </div>
+        <div className='ori__banner-call'>
+          <MdPhoneInTalk size={24} />
+          <span><a href="tel:+919100094789">9100094789</a></span>
+          <span><p> Or </p></span>
+          <span><a href="tel:+919398808712">9398808712</a></span>
+        </div>
+      </div>
       <div className='ori__navbar'>
         <div className='ori__navbar-links'>
           <Link to='/'>
@@ -27,8 +38,9 @@ const Navbar = () => {
           <NavMenu />
         </div>
         <div className='ori__navbar-contact'>
-          <a href="tel:+"><p><span><MdPhoneInTalk /></span>+91-9100094789</p></a>
-          <button type='button'>Chat</button>
+          {/* <a href="tel:+919100094789"><p><span><MdPhoneInTalk /></span>9100094789</p></a> */}
+          {/* <button type='button'>Chat</button> */}
+          <a href="tel:+919100094789"><button type='button'>Call Now</button></a>
         </div>
         <div className='ori__navbar-menu'>
           {toggleMenu 
@@ -41,8 +53,8 @@ const Navbar = () => {
               <div className='ori__navbar-menu_container-links'>
                 <NavMenu />
                 <div className='ori__navbar-menu_container-links-contact'>
-                  <a href="tel:+"><p>+91-9518947137</p></a>
-                  <button type='button'>Chat</button>
+                  <a href="tel:+919100094789"><p>9100094789</p></a>
+                  {/* <button type='button'>Chat</button> */}
                 </div>
               </div>
             </div>
