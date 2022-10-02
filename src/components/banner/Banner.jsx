ca1a224d8a1a4bc8e659e0aa90fe86e1201a113b
Nithin Gopal <nithingopal2015@gmail.com>
2022-10-02 17:03:52 +0530

    banner fix

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './banner.styles.css'
-import { TbDiscount2 } from 'react-icons/tb'
+import { MdLocalOffer } from 'react-icons/md'
 import { motion } from 'framer-motion'
 
 const bannerVariants = {
@@ -18,27 +18,14 @@ const bannerVariants = {
   }
 }
 
-const offerVariants = {
-  small: {
-    scale: 0.8
-  },
-  big: {
-    scale: 1.05,
-    transition: {
-      duration: 1,
-      yoyo: Infinity
-    }
-  }
-}
-
 const Banner = () => {
   return (
     <div className='banner-container'>
       <motion.div variants={bannerVariants} initial='hidden' animate='visible' className='banner-container_content'>
-        <p variants={offerVariants} initial='small' animate='big'>
+        <MdLocalOffer size={20} color='#00506C' />
+        <p>
           20% Discount! Insurance cover Upto 5-10 lakhs on Termite control service.
         </p>
-        <p></p>
       </motion.div>
         
     </div>
